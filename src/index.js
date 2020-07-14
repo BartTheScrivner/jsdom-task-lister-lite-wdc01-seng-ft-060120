a59fbb0706285b83d8e3861bbea8384c3c7c6e5d
BartTheScrivner <dframe22@gmail.com>
2020-07-14 16:47:56 -0400

    DONE w/ delete stretch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let itemCounter = 0
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let task = document.getElementById('new-task-description');
+    let taskList = document.getElementById('tasks');
+    let newTask = document.createElement('li');
+    let newButton = document.createElement('button');
+    newButton.innerText = "X"
+    newButton.addEventListener('click', () => {
+      newButton.parentNode.remove();
+    })
+    newTask.innerText = task.value;
+    newTask.appendChild(newButton);
+    console.log(newButton.parentNode);
+    taskList.appendChild(newTask);
+  });
+
+
 });
+
+
+
